b2ab759522a26ac87687f7039b36d3104192462e
Jean-Philippe Roemer <jp@roemer.im>
2016-05-16 06:36:22 +0100

    fish: update fishline to v3.0.0-beta & add term colors

--- fish/conf.d/21-fishline.fish
+++ fish/conf.d/21-fishline.fish
@@ -5,20 +5,12 @@
 # ---  Fishline  ---------------------------------------------------------------
 # ------------------------------------------------------------------------------
 
-if test -f "$FISH_PLUGIN_PATH/fishline/fishline.fish"
+if test -f "$FISH_PLUGIN_PATH/fishline/init.fish"
 
-    # set FLINE_PATH "$FISH_PLUGIN_PATH/fishline"
-    # source $FLINE_PATH/fishline.fish
-    set FLINE_PATH "$HOME/Projects/Fish/fishline"
-    source $FLINE_PATH/init.fish
-
-    if [ "$TERM_PROGRAM" = "Terminal-Plus" ]
-        source $FLINE_PATH/themes/git_minimal.fish
-        source $FLINE_PATH/themes/tty-compatible.fish
-    else if [ "$OS_TYPE" = "Linux" ]; and tty | grep tty > /dev/null
-        source $FLINE_PATH/themes/git_minimal.fish
-        source $FLINE_PATH/themes/tty-compatile.fish
-    end
+    set FLINE_PATH "$FISH_PLUGIN_PATH/fishline"
+    source $FLINE_PATH/fishline.fish
+    # set FLINE_PATH "$HOME/Projects/Fish/fishline"
+    # source $FLINE_PATH/init.fish
 
 end
 
