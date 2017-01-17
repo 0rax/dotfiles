aaf235f14690228ec93bc0ce2e082ef7a6771210
Jean-Philippe Roemer <jp@roemer.im>
2017-01-17 22:25:36 +0100

    atom: add terminal + vim-mode-plus

--- osx/config.sh
+++ osx/config.sh
@@ -3,6 +3,7 @@
 # Config
 SCREENSHOT_PATH=~/Pictures/Screenshots/
 HIDDEN_FILES=false
+DESKTOP_ICONS=false
 
 # Change default screenshot path
 defaults write com.apple.screencapture location ${SCREENSHOT_PATH}
@@ -13,6 +14,14 @@ if [ "${HIDDEN_FILES}" = "true" ]; then
 else
     defaults write com.apple.Finder AppleShowAllFiles false
 fi
+
+# Show desktop icons
+if [ "${DESKTOP_ICONS}" = "true" ]; then
+    defaults write com.apple.finder CreateDesktop -bool true
+else
+    defaults write com.apple.finder CreateDesktop -bool false
+fi
+
 
 # Apply all config by restart system app
 killall SystemUIServer
