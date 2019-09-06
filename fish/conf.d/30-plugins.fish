0e76f8c84e05744d3d0e7cbf695920472c2d7490
Jean-Philippe Roemer <jp@roemer.im>
2019-09-06 15:56:37 +0200

    fish: add new function to gen ssh pubkey from private key

--- fish/conf.d/30-plugins.fish
+++ fish/conf.d/30-plugins.fish
@@ -41,7 +41,8 @@ end
 
 if test -f "$FISH_PLUGIN_PATH/z/conf.d/z.fish"
     set -gx fish_function_path $fish_function_path "$FISH_PLUGIN_PATH/z/functions"
-    set -U Z_DATA $XDG_DATA_HOME/z.db
+    set -U Z_DATA $XDG_DATA_HOME/z/db
+    set -U Z_DATA_DIR $XDG_DATA_HOME/z
     set -U Z_CMD  "j"
     source "$FISH_PLUGIN_PATH/z/conf.d/z.fish"
 end
---
+++ fish/functions/ssh-pubkey.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+# -*-  mode:fish; tab-width:4  -*-
+
+function ssh-pubkey
+
+    for k in $argv
+        set -l pk (ssh-keygen -y -f $k)
+        echo "$pk $k"
+    end
+
+end
