707ec190ff122a3e7da2c856d354aab104a73455
JP Roemer <2822534+0rax@users.noreply.github.com>
2023-06-15 23:05:54 +0200

    Update power.sh icons

--- sway/sway/scripts/power.sh
+++ sway/sway/scripts/power.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 op=$(printf "img:/usr/share/icons/Papirus/48x48/apps/system-lock-screen.svg:text:Lock
-img:/usr/share/icons/Papirus/48x48/apps/system-log-out.svg:text:Logout
-img:/usr/share/icons/Papirus/48x48/apps/system-suspend.svg:text:Suspend
+img:/usr/share/icons/Papirus/48x48/apps/gnome-logout.svg:text:Logout
+img:/usr/share/icons/Papirus/48x48/apps/sleep.svg:text:Suspend
 img:/usr/share/icons/Papirus/48x48/apps/system-hibernate.svg:text:Hibernate
 img:/usr/share/icons/Papirus/48x48/apps/system-shutdown.svg:text:Poweroff
 img:/usr/share/icons/Papirus/48x48/apps/system-reboot.svg:text:Reboot" \
