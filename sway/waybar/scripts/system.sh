8a56ab4d1dc7a2bbff8df4050244e6a0b7bb1659
JP Roemer <2822534+0rax@users.noreply.github.com>
2023-07-10 02:14:04 +0200

    sway: fix system script to calculage cpu usage over a period

--- sway/waybar/scripts/system.sh
+++ sway/waybar/scripts/system.sh
@@ -1,58 +1,111 @@
 #!/usr/bin/env bash
 
-calc(){ awk "BEGIN { print $* }"; }
-
 TEMPCRITICAL=70
 THERMALZONE="/sys/class/thermal/thermal_zone7/"
 DISKMOUNT="/"
 
-## CPU
-CPUPCT=$(grep "cpu " /proc/stat | awk '{ idle = $5; total = 0; for (i=2; i<=NF; i++) { total += $i}; print (1-(idle)/(total))*100 }')
-CPUFREQ=$(grep 'cpu MHz' /proc/cpuinfo | awk -F: -v N=2 '{ sum += $N/1000 } END { if (NR > 0) print sum / NR }')
-
-## Temperature Data
-TEMPCUR=$(awk '{ print $N/1000 }' < "${THERMALZONE}/temp")
 TEMPTYPE=$(cat "${THERMALZONE}/type")
 
-## RAM
-RAMTOTAL=$(grep MemTotal /proc/meminfo | awk '{ print $2/1024/1024 }')
-RAMAVAIL=$(grep MemAvailable /proc/meminfo | awk '{ print $2/1024/1024 }')
-RAMUSED=$(calc "${RAMTOTAL} - ${RAMAVAIL}")
-RAMPCT=$(calc "${RAMUSED} / ${RAMTOTAL} * 100")
+calc(){ awk "BEGIN { print $* }"; }
 
-## Swap
-SWAPTOTAL=$(grep SwapTotal /proc/meminfo | awk '{ print $2/1024/1024 }')
-SWAPAVAIL=$(grep SwapFree /proc/meminfo | awk '{ print $2/1024/1024 }')
-SWAPUSED=$(calc "${SWAPTOTAL} - ${SWAPAVAIL}")
+cpu_active_prev=0
+cpu_total_prev=0
+cpu() {
+    # Fetch CPU data
+    read -r user nice system idle iowait softirq steal < <(awk '/cpu /{print $2" "$3" "$4" "$5" "$6" "$8" "$9}' /proc/stat)
+    cpu_active=$(calc "$user+$system+$nice+$softirq+$steal")
+    cpu_total=$(calc "$user+$system+$nice+$softirq+$steal+$idle+$iowait")
+    cpu_pct=$(calc "($cpu_active-$cpu_active_prev) / ($cpu_total-$cpu_total_prev) * 100")
+    cpu_active_prev=${cpu_active}
+    cpu_total_prev=${cpu_total}
+    cpu_freq=$(awk -F: '/cpu MHz/{ sum += $2/1000; i++ } END { if (i > 0) print sum / i }' /proc/cpuinfo)
+}
 
-## Disk
-DISKTOTAL=$(stat -f -c '%b %S' "${DISKMOUNT}" | awk '{ print ($1 * $2) / 1024 / 1024 / 1024 }')
-DISKAVAIL=$(stat -f -c '%a %S' "${DISKMOUNT}" | awk '{ print ($1 * $2) / 1024 / 1024 / 1024 }')
-DISKUSED=$(calc "${DISKTOTAL} - ${DISKAVAIL}")
+temperature() {
+    # Fetch Temperature data
+    temp=$(awk '{ print $N/1000 }' "${THERMALZONE}/temp")
+}
 
-## Waybar output
-TEXT=$(printf "󰢻 %.0f%%  %d°C 󱘲 %.0f%%\n" "${CPUPCT}" "${TEMPCUR}" "${RAMPCT}")
-TOOLTIP=$(printf "󰢻 %02.1f%% (%.3fGhz)
- %d°C (%s)
+memory() {
+    # Fetch RAM data
+    ram_total=$(awk '/MemTotal/{ print $2/1024/1024 }' /proc/meminfo)
+    ram_avail=$(awk '/MemAvailable/{ print $2/1024/1024 }' /proc/meminfo)
+    ram_used=$(calc "${ram_total} - ${ram_avail}")
+    ram_pct=$(calc "${ram_used} / ${ram_total} * 100")
+}
+
+swap() {
+    # Fetch Swap data
+    swap_total=$(awk '/SwapTotal/{ print $2/1024/1024 }' /proc/meminfo)
+    swap_avail=$(awk '/SwapFree/{ print $2/1024/1024 }' /proc/meminfo)
+    swap_used=$(calc "${swap_total} - ${swap_avail}")
+}
+
+disk() {
+    # Fetch Disk data
+    disk_total=$(stat -f -c '%b %S' "${DISKMOUNT}" | awk '{ print ($1 * $2) / 1024 / 1024 / 1024 }')
+    disk_avail=$(stat -f -c '%a %S' "${DISKMOUNT}" | awk '{ print ($1 * $2) / 1024 / 1024 / 1024 }')
+    disk_used=$(calc "${disk_total} - ${disk_avail}")
+}
+
+output() {
+    # Generate output variables
+    text=$(printf "󰢻 %.0f%%  %d°C 󱘲 %.0f%%\n" "${cpu_pct}" "${temp}" "${ram_pct}")
+    tooltip=$(printf "󰢻 %02.1f%% (%.3fGhz)
+ %d°C (${TEMPTYPE})
 󱘲 %.1fGB/%.1fGB
 󰾴 %.1fGB/%.1fGB
-󰋊 %.1fGB/%.1fGB" "${CPUPCT}" "${CPUFREQ}" "${TEMPCUR}" "${TEMPTYPE}" "${RAMUSED}" "${RAMTOTAL}" "${SWAPUSED}" "${SWAPTOTAL}" "${DISKUSED}" "${DISKTOTAL}")
-CLASS=""
+󰋊 %.1fGB/%.1fGB" "${cpu_pct}" "${cpu_freq}" "${temp}" "${ram_used}" "${ram_total}" "${swap_used}" "${swap_total}" "${disk_used}" "${disk_total}")
+    class=""
+    if [[ ${temp} -ge ${TEMPCRITICAL} ]]; then
+        class="critical"
+    fi
+}
+
+status() {
+    # Print system status once (shows average CPU usage since boot)
+    cpu
+    temperature
+    memory
+    swap
+    disk
+    output
+    jq --null-input --compact-output '{"text":$text, "tooltip":$tooltip, "class":$class}' \
+                --arg text "${text}" \
+                --arg tooltip "${tooltip}" \
+                --arg class "${class}"
+}
+
+watch() {
+    # Print system status periodically (calculate CPU usage from each cycle)
+    while true; do
+        status
+        sleep 3
+    done
+}
+
+raw() {
+    # Print a raw version of status
+    cpu
+    temperature
+    memory
+    swap
+    disk
+    output
+    echo "${text}"
+    echo "------------------"
+    echo "${tooltip}"
+}
 
 CMD="${1:-status}"
 case $CMD in
 status)
-    if [[ ${TEMPCUR} -ge ${TEMPCRITICAL} ]]; then
-        CLASS="critical"
-    fi
-    jq --null-input --compact-output '{"text":$text, "tooltip":$tooltip, "class":$class}' \
-                --arg text "${TEXT}" \
-                --arg tooltip "${TOOLTIP}" \
-                --arg class "${CLASS}"
+    status
+    ;;
+watch)
+    watch
     ;;
 raw)
-    echo "${TEXT}"
-    echo "------------------"
-    echo "${TOOLTIP}"
+    raw
     ;;
 esac
